3e0826a3e28ae990ef201458ad4c176582a3099f
GodWasaProgrammer <bwnils@gmail.com>
2024-06-26 00:16:10 +0200

    Add project files.

---
+++ ccwc/CLIMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ccwc;
+
+internal class CLIMethods
+{
+    public static string CountLines(string path)
+    {
+        int count = 0;
+        using (StreamReader sr = new StreamReader(path))
+        {
+            while (sr.ReadLine() != null)
+            {
+                count++;
+            }
+        }
+        return $"{count} {path}";
+    }
+}
---
+++ ccwc/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ccwc;
+
+internal class Program
+{
+    static void Main()
+    {
+        do
+        {
+            var lineInput = Console.ReadLine();
+
+            lineInput.ToLower();
+
+            if (lineInput.Contains("ccwc"))
+            {
+                var extractedfilename = lineInput.Remove(0, 8);
+                var fileinfo = new FileInfo(extractedfilename);
+                if (fileinfo.Exists)
+                {
+                    if (lineInput.Contains("-c"))
+                    {
+
+                        var fileBytes = fileinfo.Length;
+                        Console.WriteLine($"{fileBytes} {extractedfilename}");
+                    }
+                    if (lineInput.Contains("-l"))
+                    {
+                        Console.WriteLine(CLIMethods.CountLines(extractedfilename));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File does not exist");
+                }
+            }
+        } while (true);
+    }
+}
